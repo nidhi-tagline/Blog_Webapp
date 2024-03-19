d814c9296bcdf2f40551f714a306f2869400bfe6
nidhi tagline <nidhig.tagline@gmail.com>
2024-03-19 12:18:16 +0530

    Models implementation

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from author.models import Author
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_add_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __init__(self):
+        return self.title
+    
+class Comment(models.Models):
+    post = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    comment_description = models.CharField(max_length=300)
+    created_by = models.ForeignKey(Author)
+    created_at = models.DateTimeField(auto_now=True)
--- blog_app/settings.py
+++ blog_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'author.apps.AuthorConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
