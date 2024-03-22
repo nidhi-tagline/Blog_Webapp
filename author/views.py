86239ba778d2637c4208f3ac81bdfb3123b3824a
nidhi tagline <nidhig.tagline@gmail.com>
2024-03-22 15:57:07 +0530

    added pagination

--- author/views.py
+++ author/views.py
@@ -1,26 +1,23 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from .models import Author
 from django.core.paginator import Paginator
 
-class BloggerListView(generic.ListView):
+class BloggerListView(ListView):
+    queryset = Author.objects.all()
     template_name = 'author/all_bloggers.html'
     context_object_name = 'authors'
+    paginate_by = 10
     
-    def get_queryset(self):
-        return Author.objects.all()
     
 class BloggerDetailView(DetailView):
     queryset = Author.objects.prefetch_related('blogs')
     template_name = "author/blogger_detail.html"
     context_object_name = "author"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         blog_list = self.queryset.get(id=self.kwargs['pk']).blogs.order_by('-created_at')
-        
+
         page_number = self.request.GET.get('page',1)
         page_size = self.request.GET.get('page_size',3)
         paginator = Paginator(blog_list, page_size)
