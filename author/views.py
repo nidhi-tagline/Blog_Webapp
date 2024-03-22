63b5785d7c31955ac2d30cfe9219310a611eb731
nidhi tagline <nidhig.tagline@gmail.com>
2024-03-22 15:08:56 +0530

    changes in blogger-detail view

--- author/views.py
+++ author/views.py
@@ -3,19 +3,17 @@ from .models import Author
 from django.core.paginator import Paginator
 
 class BloggerDetailView(generic.DetailView):
-    queryset = Author.objects.all()
+    queryset = Author.objects.prefetch_related('blogs')
     template_name = "author/blogger_detail.html"
     context_object_name = "author"
     
-    def get_queryset(self):
-        return self.queryset.filter(id=self.kwargs['pk'])
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        author = self.get_object()
-        queryset = author.blogs.order_by('-created_at')        
-        paginator = Paginator(queryset, 3)
-        page_number = self.request.GET.get('page')
+        blog_list = self.queryset.get(id=self.kwargs['pk']).blogs.order_by('-created_at')
+        
+        page_number = self.request.GET.get('page',1)
+        page_size = self.request.GET.get('page_size',3)
+        paginator = Paginator(blog_list, page_size)
         page_obj = paginator.get_page(page_number)
         context['author_posts'] = page_obj
         return context
