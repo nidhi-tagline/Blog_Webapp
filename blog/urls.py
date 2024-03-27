3e5351b2f4199ad5c0d1ae4675ff461fd102d68c
nidhi tagline <nidhig.tagline@gmail.com>
2024-03-27 10:58:36 +0530

    created blog-detail page and add-comment page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name = "blog"
 urlpatterns = [
     path('', views.IndexView.as_view() ,name="home"),
     path('blogs/', views.AllBlogView.as_view(), name='all-blogs'),
+    path('blogs/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail'),
+    path('blogs/<int:pk>/create/', views.AddCommentView.as_view(), name='add-comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
-from django.views import generic
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .models import Blog, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 
 
-class IndexView(generic.TemplateView):
+class IndexView(TemplateView):
     template_name = "blog/index.html"
 
-class AllBlogView(generic.ListView):
+class AllBlogView(ListView):
     template_name = "blog/all_blogs.html"
     paginate_by = 5
     context_object_name = 'blog_posts' 
@@ -13,3 +15,33 @@ class AllBlogView(generic.ListView):
     def get_queryset(self):
         return Blog.objects.all().order_by("-created_at")
         
+class BlogDetailView(DetailView):
+    queryset = Blog.objects.select_related('author')
+    template_name = "blog/blog_detail.html"
+    context_object_name = 'post'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_comments'] = self.queryset.get(pk=self.kwargs['pk']).comments.order_by("created_at")
+        return context
+    
+class AddCommentView(LoginRequiredMixin, CreateView):
+    template_name = "blog/add_comment.html"
+    model = Comment
+    fields = ['comment',]
+    
+    def get_context_data(self, **kwargs):
+        context = super(AddCommentView,self).get_context_data(**kwargs)
+        context['post'] = Blog.objects.get(pk=self.kwargs['pk'])
+        return context
+    
+    def form_valid(self, form):
+        new_comment = form.save(commit=False)
+        post = Blog.objects.get(id=self.kwargs["pk"])
+        new_comment.created_by = self.request.user
+        new_comment.post = post
+        new_comment.save(force_insert=True)
+        return super().form_valid(form)
+    
+    def get_success_url(self):  
+        return reverse("blog:blog-detail", kwargs={"pk": self.kwargs["pk"],})
