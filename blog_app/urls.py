d1287eed245c6cfcf58a1b8750682064bd91b663
nidhi tagline <nidhig.tagline@gmail.com>
2024-03-28 11:54:03 +0530

    set DEBUG_TOOLBAR variable

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,11 +1,15 @@
 
 from django.contrib import admin
 from django.urls.conf import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include("blog.urls")),
     path('blogs/bloggers/', include("author.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG_TOOLBAR :
+    urlpatterns += [
+        path('__debug__/', include("debug_toolbar.urls")),
+    ]
