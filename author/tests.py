fcf12d9c7c4b6ee0e03ceb27b4a9535a19f80d6f
nidhi tagline <nidhig.tagline@gmail.com>
2024-04-01 17:46:35 +0530

    test: added new testcases

--- author/tests.py
+++ author/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test.testcases import TestCase
+from django.test.client import Client
 from .models import Author
+from django.urls.base import reverse
 
-class AuthorModelTest(TestCase):
+class AuthorModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         Author.objects.create(username="Test_author",bio_detail="This is sample bio")
@@ -28,4 +30,40 @@ class AuthorModelTest(TestCase):
         author = self.get_author()
         expected_name = author.username
         self.assertEqual(expected_name, str(author))
+        
+    # check if author username is correct
+    def test_author_username(self):
+        author = self.get_author()
+        expected_name = "Test_author"
+        self.assertEqual(expected_name, author.username)
+        
+    # check if author bio detail is correct
+    def test_author_bio_detail(self):
+        author = self.get_author()
+        expected_bio = "This is sample bio"
+        self.assertEqual(author.bio_detail, expected_bio)
+        
+class AuthorProfileUpdateTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Author.objects.create(username="Test_author",bio_detail="This is sample bio")
+        cls.client = Client()
+        
+    # check if authenticated user can update profile page
+    def test_profile_update_for_authenticated_user(self):
+        author = Author.objects.get(id=1)
+        self.client.force_login(author)
+        response = self.client.post(reverse("author:author-profile-update",kwargs={"pk": author.pk}),{'username':'Test_author','bio_detail':'updated bio'})
+        self.assertEqual(response.status_code, 302)
+        
+        author.refresh_from_db()
+        self.assertEqual(author.bio_detail,"updated bio")
+        
+    # check if other user cannot update profile
+    def test_update_profile_not_accessible(self):
+        author = Author.objects.get(id=1)
+        self.client.post(reverse("author:author-profile-update",kwargs={"pk": author.pk}),{'username':'Test_author','bio_detail':'updated bio'})
+        
+        author.refresh_from_db()
+        self.assertNotEqual(author.bio_detail,"updated bio")
         
--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,12 @@
 from django.urls.base import reverse
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import Blog, Author, Comment
 from datetime import datetime
 
 class BlogModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_author = Author.objects.create(username="test_auther",bio_detail="This is a bio for test auther.")
+        test_author = Author.objects.create(username="test_author",bio_detail="This is a bio for test auther.")
         Blog.objects.create(title="sample blog",content="This is a sample blog",author=test_author)
 
     def get_blog(self):
@@ -37,10 +37,16 @@ class BlogModelTests(TestCase):
         field_name = blog._meta.get_field('content').verbose_name
         self.assertEqual(field_name,'content')
         
+    # check whether created blog has correct author.
+    def test_correct_author(self):
+        blog = self.get_blog()
+        expected_author = "test_author"
+        self.assertEqual(blog.author.username, expected_author)
+        
 class CommentModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_author1 = Author.objects.create(username="test_auther1", bio_detail="This is a bio for test auther1.")
+        test_author1 = Author.objects.create(username="test_author1", bio_detail="This is a bio for test author1.")
         test_author2 = Author.objects.create(username="testuser2", bio_detail="I am just a user")
         blog = Blog.objects.create(title="sample blog", content="This is a sample blog", author=test_author1)
         Comment.objects.create(comment="this is test comment", post=blog, created_by=test_author2)
@@ -67,7 +73,7 @@ class CommentModelTests(TestCase):
 class BlogListViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_author = Author.objects.create(username="test_auther",bio_detail="This is a bio for test auther.")
+        test_author = Author.objects.create(username="test_author",bio_detail="This is a bio for test author.")
         blog_list = 10
         for i in range(blog_list):
             Blog.objects.create(title=f"Blog {i}",content=f"Content of the blog {i}",author=test_author)
@@ -88,3 +94,22 @@ class BlogListViewTests(TestCase):
         response = self.client.get(reverse('blog:all-blogs'))
         self.assertTrue(response.context['is_paginated'],True)
         self.assertEqual(len(response.context['blog_posts']),5)
+        
+class AddCommentViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_author1 = Author.objects.create(username="test_author1", bio_detail="This is a bio for test author1.")
+        Blog.objects.create(title="sample blog", content="This is a sample blog", author=test_author1)
+        cls.client = Client()
+        
+    # check that only authenticated user can add comment
+    def test_authenticated_user_can_add_comment(self):
+        test_user = Author.objects.create(username="testuser", bio_detail="I am just a user")
+        self.client.force_login(test_user)
+        blog = Blog.objects.get(id=1)
+        response = self.client.post(reverse('blog:add-comment',kwargs={"pk":blog.pk}),{'comment':'test comment'})
+        self.assertEqual(response.status_code,302)
+        
+        comment = Comment.objects.filter(comment="test comment")
+        self.assertTrue(comment)
+        
