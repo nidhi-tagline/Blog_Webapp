c91184ffaae312049c2fcdcce072e342fd117248
nidhi tagline <nidhig.tagline@gmail.com>
2024-04-04 16:40:06 +0530

    added allowed-hosts in .env file

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS')]
 
 DEBUG_TOOLBAR = 0
 # Application definition
